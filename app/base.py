36387a11a265b5f457f208252cd6f3aac3e390c0
Sheikh Muhammad Abdullah <sheikhmuhammad2002@gmail.com>
2025-10-11 22:05:00 +0600

    pushed the project structure

---
+++ app/base.py
@@ -0,0 +1,4 @@
+from db.base_class import Base
+
+# from db.model.blog import Blog
+# from db.model.user import User
---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from core.config import settings
+from db.session import engine
+from db.base import Base
+
+from apis.base import api_router
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def include_router(app):
+    app.include_router(api_router)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    include_router(app)
+    return app
+
+
+app = start_application()
